bb3cffc455c622afadf88e33bd794850ef3790df
Alex Nagel <awnagel700@gmail.com>
2019-12-27 22:39:08 +0100

    Added from and to lerp functions for fading.
    Still need to add rotation, but the matrix rotations don't work with the positions alterations.

--- TextLerpEffects.cs
+++ TextLerpEffects.cs
@@ -14,12 +14,12 @@ public class TextLerpEffects : MonoBehaviour
         text = GetComponent<TMP_Text>();
         //text.color = new Color(text.color.r, text.color.g, text.color.b, 0);
     }
-    
-    public IEnumerator LerpText(Vector3 offset, float time, bool delayWord = false, bool delayCharacter = false, float wordDelayTime = 0.4f)
+
+    public IEnumerator LerpText(Vector3 offset, float time, bool fade = false, bool delayWord = false, bool delayCharacter = false, float wordDelayTime = 0.4f)
     {
         for (int i = 0; i < text.textInfo.wordCount; i++)
         {
-            StartCoroutine(LerpWord(i, offset, time, delayCharacter));
+            StartCoroutine(LerpWord(i, offset, time, fade, delayCharacter, 0.025f));
 
             if (delayWord)
                 yield return new WaitForSeconds(wordDelayTime);
@@ -28,13 +28,20 @@ public class TextLerpEffects : MonoBehaviour
         yield return null;
     }
 
-    public IEnumerator LerpWord(int wordIndex, Vector3 offset, float time, bool delay = false, float characterDelaySpeed = 0.025f)
+    public IEnumerator LerpWord(int wordIndex, Vector3 offset, float time, bool fade = false, bool delay = false, float characterDelaySpeed = 0.025f)
     {
         TMP_WordInfo info = text.textInfo.wordInfo[wordIndex];
 
         for (int i = 0; i < info.characterCount; i++)
         {
-            StartCoroutine(LerpCharacter(info.firstCharacterIndex + i, offset, time));
+            if (fade)
+            {
+                StartCoroutine(LerpCharacterFrom(info.firstCharacterIndex + i, offset, time));
+            }
+            else
+            {
+                StartCoroutine(LerpCharacterTo(info.firstCharacterIndex + i, offset, time));
+            }
 
             if (delay)
                 yield return new WaitForSeconds(characterDelaySpeed);
@@ -43,7 +50,7 @@ public class TextLerpEffects : MonoBehaviour
         yield return null;
     }
 
-    public IEnumerator LerpCharacter(int index, Vector3 offset, float time)
+    public IEnumerator LerpCharacterFrom(int index, Vector3 offset, float time)
     {
         text.ForceMeshUpdate();
 
@@ -63,22 +70,71 @@ public class TextLerpEffects : MonoBehaviour
 
             Vector3[] destinationVertices = text.textInfo.meshInfo[materialIndex].vertices;
 
-            destinationVertices[vertexIndex] = sourceVertices[vertexIndex] - offset;
-            destinationVertices[vertexIndex + 1] = sourceVertices[vertexIndex + 1] - offset;
-            destinationVertices[vertexIndex + 2] = sourceVertices[vertexIndex + 2] - offset;
-            destinationVertices[vertexIndex + 3] = sourceVertices[vertexIndex + 3] - offset;
+            destinationVertices[vertexIndex] = sourceVertices[vertexIndex] + offset;
+            destinationVertices[vertexIndex + 1] = sourceVertices[vertexIndex + 1] + offset;
+            destinationVertices[vertexIndex + 2] = sourceVertices[vertexIndex + 2] + offset;
+            destinationVertices[vertexIndex + 3] = sourceVertices[vertexIndex + 3] + offset;
 
             Color32[] newColors = text.textInfo.meshInfo[materialIndex].colors32;
             
-            newColors[vertexIndex].a = (byte)Mathf.Lerp(0, 255, Mathf.SmoothStep(0.0f, 1.0f, t));
+            newColors[vertexIndex + 0].a = (byte)Mathf.Lerp(0, 255, Mathf.SmoothStep(0.0f, 1.0f, t));
             newColors[vertexIndex + 1].a = (byte)Mathf.Lerp(0, 255, Mathf.SmoothStep(0.0f, 1.0f, t));
             newColors[vertexIndex + 2].a = (byte)Mathf.Lerp(0, 255, Mathf.SmoothStep(0.0f, 1.0f, t));
             newColors[vertexIndex + 3].a = (byte)Mathf.Lerp(0, 255, Mathf.SmoothStep(0.0f, 1.0f, t));
 
-            destinationVertices[vertexIndex] = Vector3.Lerp(destinationVertices[vertexIndex], sourceVertices[vertexIndex], Mathf.SmoothStep(0.0f, 1.0f, t));
+            destinationVertices[vertexIndex + 0] = Vector3.Lerp(destinationVertices[vertexIndex + 0], sourceVertices[vertexIndex + 0], Mathf.SmoothStep(0.0f, 1.0f, t));
             destinationVertices[vertexIndex + 1] = Vector3.Lerp(destinationVertices[vertexIndex + 1], sourceVertices[vertexIndex + 1], Mathf.SmoothStep(0.0f, 1.0f, t));
             destinationVertices[vertexIndex + 2] = Vector3.Lerp(destinationVertices[vertexIndex + 2], sourceVertices[vertexIndex + 2], Mathf.SmoothStep(0.0f, 1.0f, t));
             destinationVertices[vertexIndex + 3] = Vector3.Lerp(destinationVertices[vertexIndex + 3], sourceVertices[vertexIndex + 3], Mathf.SmoothStep(0.0f, 1.0f, t));
+
+            text.UpdateVertexData(TMP_VertexDataUpdateFlags.All);
+
+            for (int i = 0; i < text.textInfo.meshInfo.Length; i++)
+            {
+                text.textInfo.meshInfo[i].mesh.vertices = text.textInfo.meshInfo[i].vertices;
+                text.UpdateGeometry(text.textInfo.meshInfo[i].mesh, i);
+            }
+
+            yield return null;
+        }
+    }
+
+    public IEnumerator LerpCharacterTo(int index, Vector3 offset, float time)
+    {
+        text.ForceMeshUpdate();
+
+        float r = 1.0f / time;
+        float t = 0.0f;
+
+        TMP_MeshInfo[] cachedMeshInfo = text.textInfo.CopyMeshInfoVertexData();
+
+        while (t < 1.0f)
+        {
+            t += Time.deltaTime * r;
+
+            int materialIndex = text.textInfo.characterInfo[index].materialReferenceIndex;
+            int vertexIndex = text.textInfo.characterInfo[index].vertexIndex;
+
+            Vector3[] sourceVertices = cachedMeshInfo[materialIndex].vertices;
+
+            Vector3[] destinationVertices = text.textInfo.meshInfo[materialIndex].vertices;
+
+            destinationVertices[vertexIndex] = sourceVertices[vertexIndex] + offset;
+            destinationVertices[vertexIndex + 1] = sourceVertices[vertexIndex + 1] + offset;
+            destinationVertices[vertexIndex + 2] = sourceVertices[vertexIndex + 2] + offset;
+            destinationVertices[vertexIndex + 3] = sourceVertices[vertexIndex + 3] + offset;
+
+            Color32[] newColors = text.textInfo.meshInfo[materialIndex].colors32;
+
+            newColors[vertexIndex + 0].a = (byte)Mathf.Lerp(255, 0, Mathf.SmoothStep(0.0f, 1.0f, t));
+            newColors[vertexIndex + 1].a = (byte)Mathf.Lerp(255, 0, Mathf.SmoothStep(0.0f, 1.0f, t));
+            newColors[vertexIndex + 2].a = (byte)Mathf.Lerp(255, 0, Mathf.SmoothStep(0.0f, 1.0f, t));
+            newColors[vertexIndex + 3].a = (byte)Mathf.Lerp(255, 0, Mathf.SmoothStep(0.0f, 1.0f, t));
+
+            destinationVertices[vertexIndex + 0] = Vector3.Lerp(sourceVertices[vertexIndex + 0], destinationVertices[vertexIndex + 0], Mathf.SmoothStep(0.0f, 1.0f, t));
+            destinationVertices[vertexIndex + 1] = Vector3.Lerp(sourceVertices[vertexIndex + 1], destinationVertices[vertexIndex + 1], Mathf.SmoothStep(0.0f, 1.0f, t));
+            destinationVertices[vertexIndex + 2] = Vector3.Lerp(sourceVertices[vertexIndex + 2], destinationVertices[vertexIndex + 2], Mathf.SmoothStep(0.0f, 1.0f, t));
+            destinationVertices[vertexIndex + 3] = Vector3.Lerp(sourceVertices[vertexIndex + 3], destinationVertices[vertexIndex + 3], Mathf.SmoothStep(0.0f, 1.0f, t));
 
             text.UpdateVertexData(TMP_VertexDataUpdateFlags.All);
 
